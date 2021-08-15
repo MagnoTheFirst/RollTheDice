27443f7f1848b3c14feec2070af7646e8d94f7af
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-08-15 13:14:51 +0200

    First Lady

---
+++ src/main/java/com/example/rollthedice/RollTheDiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rollthedice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollTheDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RollTheDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rollthedice/RollTheDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rollthedice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollTheDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
